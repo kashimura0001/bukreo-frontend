583690ed8edebd8b30a60399bb21e72148d9e76d
keisukekashimura <kashimura@refcome.com>
2021-05-02 23:22:05 +0900

    fix team list style

--- src/components/TeamList.tsx
+++ src/components/TeamList.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { TeamListFragment, UserRole } from "../graphql/schema";
-import { Box, Flex, Text, SimpleGrid } from "@chakra-ui/react";
+import { Box, Flex, Text, SimpleGrid, Icon } from "@chakra-ui/react";
 import { generatePath, Link } from "react-router-dom";
 import { TIMELINE_PATH } from "../config/routes";
 import ContentLoader from "react-content-loader";
@@ -9,12 +9,19 @@ const TeamListContentLoader: FC = () => {
   return (
     <Box width="100%">
       <ContentLoader width="100%" height="100">
-        <rect x="0" y="0" width="260" height="100" />
-        <rect x="320" y="0" width="260" height="100" />
-        <rect x="640" y="0" width="260" height="100" />
+        <rect x="0" y="0" width="100%" height="100" />
       </ContentLoader>
     </Box>
   );
+};
+
+const userRoleText = (role: UserRole) => {
+  switch (role) {
+    case UserRole.Admin:
+      return "管理者";
+    case UserRole.Member:
+      return "メンバー";
+  }
 };
 
 type Props = {
@@ -27,29 +34,28 @@ export const TeamList: FC<Props> = ({ members }) => {
   }
 
   return (
-    <SimpleGrid columns={3} spacing="60px">
+    <SimpleGrid spacing="30px">
       {members.map(({ id, role, team }) => (
         <Link key={id} to={generatePath(TIMELINE_PATH, { teamId: team.id })}>
-          <Box
+          <Flex
+            w="100%"
             h="100px"
             p="20px"
-            boxShadow="md"
             border="1px"
             borderRadius="10px"
-            borderColor="gray.400"
-            _hover={{ backgroundColor: "gray.50" }}
+            borderColor="gray.300"
+            alignItems="center"
+            _hover={{ backgroundColor: "gray.100" }}
           >
-            <Text h="50px" overflow="hidden" fontWeight="bold">
+            {/* TODO チームアイコンを設定できるようにする */}
+            <Box borderRadius="50%" backgroundColor="gray.200" w="45px" h="45px" />
+            <Text ml="20px" fontWeight="bold" overflow="hidden" isTruncated>
               {team.name}
             </Text>
-            {role === UserRole.Admin && (
-              <Flex justifyContent="flex-end">
-                <Text mt="5px" fontSize="13px">
-                  管理者
-                </Text>
-              </Flex>
-            )}
-          </Box>
+            <Text ml="auto" fontSize="12px">
+              {role ? userRoleText(role) : ""}
+            </Text>
+          </Flex>
         </Link>
       ))}
     </SimpleGrid>
