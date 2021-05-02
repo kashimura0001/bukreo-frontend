1bad343ef411eb1a2534bc2b78725fda6ab315d9
keisukekashimura <kashimura@refcome.com>
2021-05-02 23:31:07 +0900

    add menu list

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,30 +1,40 @@
 import React, { FC } from "react";
-import { Flex, Box, Heading, Spacer, Button, Avatar } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Spacer,
+  Button,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+} from "@chakra-ui/react";
 
 type Props = {
   avatarUrl: string;
 };
 
 export const MainHeader: FC<Props> = ({ avatarUrl }) => {
-  const handleClickIcon = () => {
-    // TODO アイコンを押下したときの処理を実装する
-    alert("hogehoge");
-  };
-
   return (
     <Flex p="2" h="60px" borderBottom="1px" borderColor="gray.300">
       <Box>
         <Heading size="lg">Bukreo</Heading>
       </Box>
       <Spacer />
-      <Box>
-        <Button borderRadius="full" p="0" onClick={handleClickIcon}>
-          <Avatar
-            boxSize="40px"
-            src={avatarUrl || "https://bit.ly/broken-link"}
-          />
-        </Button>
-      </Box>
+      <Menu>
+        <MenuButton as={Button} borderRadius="full" p="0">
+          <Avatar boxSize="40px" src={avatarUrl || "https://bit.ly/broken-link"} />
+        </MenuButton>
+        <MenuList>
+          <MenuItem>プロフィール</MenuItem>
+          <MenuItem>チームリスト</MenuItem>
+          <MenuDivider />
+          <MenuItem color="red">ログアウト</MenuItem>
+        </MenuList>
+      </Menu>
     </Flex>
   );
 };
