862caf4b518af9b32bde3c76beefb7472c337ed6
keisukekashimura <kashimura@refcome.com>
2021-05-03 10:35:18 +0900

    make it possible to move to team list

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -12,12 +12,17 @@ import {
   MenuItem,
   MenuDivider,
 } from "@chakra-ui/react";
-
+import { FiUser, FiSettings, FiList, FiLogOut } from "react-icons/fi";
+import { useParams, useHistory } from "react-router-dom";
+import { TEAMS_PATH } from "../config/routes";
 type Props = {
   avatarUrl: string;
 };
 
 export const MainHeader: FC<Props> = ({ avatarUrl }) => {
+  const { teamId }: { teamId: string } = useParams();
+  const history = useHistory();
+
   return (
     <Flex p="2" h="60px" borderBottom="1px" borderColor="gray.300">
       <Box>
@@ -26,13 +31,23 @@ export const MainHeader: FC<Props> = ({ avatarUrl }) => {
       <Spacer />
       <Menu>
         <MenuButton as={Button} borderRadius="full" p="0">
-          <Avatar boxSize="40px" src={avatarUrl || "https://bit.ly/broken-link"} />
+          <Avatar boxSize="40px" src={avatarUrl || ""} />
         </MenuButton>
         <MenuList>
-          <MenuItem>プロフィール</MenuItem>
-          <MenuItem>チームリスト</MenuItem>
+          <MenuItem icon={<FiUser />}>プロフィール</MenuItem>
+          {teamId && (
+            <>
+              <MenuDivider />
+              <MenuItem icon={<FiSettings />}>チーム設定</MenuItem>
+              <MenuItem icon={<FiList />} onClick={() => history.push(TEAMS_PATH)}>
+                チームリスト
+              </MenuItem>
+            </>
+          )}
           <MenuDivider />
-          <MenuItem color="red">ログアウト</MenuItem>
+          <MenuItem icon={<FiLogOut />} color="red">
+            ログアウト
+          </MenuItem>
         </MenuList>
       </Menu>
     </Flex>
