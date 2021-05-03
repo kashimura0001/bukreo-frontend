b9b5181bd544fc02c212af44fe9d79e110e65272
keisukekashimura <kashimura@refcome.com>
2021-05-03 10:18:51 +0900

    add main header

--- src/screens/TimelineScreen.tsx
+++ src/screens/TimelineScreen.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from "react";
 import { useParams } from "react-router-dom";
+import { MainHeader } from "../components/MainHeader";
 
 type Props = {};
 
 export const TimelineScreen: FC<Props> = () => {
   const { teamId } = useParams<{ teamId: string }>();
-  return <div>TimelineScreen: {teamId}</div>;
+  return (
+    <>
+      <MainHeader avatarUrl={""} />
+      <div>TimelineScreen: {teamId}</div>
+    </>
+  );
 };
