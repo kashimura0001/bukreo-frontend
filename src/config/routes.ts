4d7ed369e0a4d9fa31061689bfe0f2a457879a83
keisukekashimura <kashimura@refcome.com>
2021-05-04 13:59:33 +0900

    add team setting screen path

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -5,4 +5,5 @@ export const PASSWORD_RESET_PATH = "/password/reset";
 export const ONBOARDING_PATH = "/onboarding";
 // private pages
 export const TEAMS_PATH = "/teams";
+export const TEAM_SETTING_PATH = "/teams/:teamId/setting"
 export const TIMELINE_PATH = "/teams/:teamId"
