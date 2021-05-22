b6db1833b0c20a7150941d2d80c18496145d5ec5
keisukekashimura <kashimura@refcome.com>
2021-05-22 23:17:26 +0900

    add color.ts

---
+++ src/constants/color.ts
@@ -0,0 +1,20 @@
+const pallets = {
+  white: "#ffffff",
+  whiteSmoke: "#fafafa",
+  lightGrey: "#cfcfcf",
+  dimGray: "#696969",
+  steelBlur: "#5786bc",
+  red: "red",
+};
+
+const semantics = {
+  bodyBackground: pallets.whiteSmoke,
+  itemBackground: pallets.white,
+  subText: pallets.dimGray,
+  whiteText: pallets.white,
+  dangerText: pallets.red,
+  border: pallets.lightGrey,
+  successButton: pallets.steelBlur,
+};
+
+export const colors = { pallets, semantics };
