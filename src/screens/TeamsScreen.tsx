81c89cb13d1c2d06d48d3f784b685a41026cc53f
keisukekashimura <kashimura@refcome.com>
2021-05-23 12:40:26 +0900

    fix team list

--- src/screens/TeamsScreen.tsx
+++ src/screens/TeamsScreen.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useState } from "react";
 import { TeamsScreenDocument, useCreateTeamMutation, useTeamsScreenQuery } from "../graphql/schema";
 import { MainHeader } from "../components/MainHeader";
-import { TeamListHeader } from "../components/TeamListHeader";
-import { Box, Flex, SimpleGrid, Skeleton } from "@chakra-ui/react";
+import { Box, Flex, HStack, Stack, Button, Avatar } from "@chakra-ui/react";
 import { TeamCreateModal } from "../components/TeamCreateModal";
 import { useToast } from "../hooks/useToast";
-import { TeamListRow } from "../components/TeamListRow";
+import { colors } from "../constants/color";
+import { generatePath, Link } from "react-router-dom";
+import { TIMELINE_PATH } from "../config/routes";
 
 type Props = {};
 
@@ -22,6 +23,7 @@ export const TeamsScreen: FC<Props> = () => {
       cache.writeQuery({ query: TeamsScreenDocument, data: { currentUser: { teams: data.createTeam } } });
     },
   });
+
   const teams = data?.currentUser.teams;
 
   const handleChangeTeamName = (value: string) => {
@@ -55,18 +57,55 @@ export const TeamsScreen: FC<Props> = () => {
   return (
     <>
       <MainHeader />
-      <Flex w="full" justifyContent="center" mt="100px">
-        <Box w="900px">
-          <TeamListHeader onCreateTeamButton={handleOpenModal} />
-          <SimpleGrid my="30px" spacing="30px">
-            {teams ? (
+      <Flex w="full" justifyContent="center" mt="200px">
+        <Box
+          w="400px"
+          bgColor={colors.semantics.itemBackground}
+          border="1px"
+          borderRadius="5px"
+          borderColor={colors.semantics.border}
+          boxShadow="md"
+        >
+          <Box
+            p="20px"
+            borderBottom="1px"
+            borderColor={colors.semantics.border}
+            color={colors.semantics.subText}
+          >{`${data?.currentUser.name} さんのチーム`}</Box>
+          <Stack px="30px">
+            {teams &&
               teams.map((team) => {
-                return team && <TeamListRow key={team.id} team={team} />;
-              })
-            ) : (
-              <Skeleton w="100%" h="100px" borderRadius="10px" />
-            )}
-          </SimpleGrid>
+                if (!team) return;
+                return (
+                  <Link key={team.id} to={generatePath(TIMELINE_PATH, { teamId: team.id })}>
+                    <HStack
+                      w="100%"
+                      p="20px"
+                      borderBottom="1px"
+                      borderColor={colors.semantics.border}
+                      _hover={{ fontWeight: "bold" }}
+                    >
+                      {/* TODO: チームのアイコンを設定する*/}
+                      <Avatar
+                        name={team.name}
+                        minW="45px"
+                        minH="45px"
+                        backgroundColor={colors.semantics.defaultIconBackground}
+                        color={colors.semantics.whiteText}
+                        fontSize="20px"
+                        fontWeight="bold"
+                      />
+                      <Box pl="15px">{team.name}</Box>
+                    </HStack>
+                  </Link>
+                );
+              })}
+          </Stack>
+          <Box textAlign="center" mt="50px" mb="20px">
+            <Button color={colors.semantics.linkText} fontWeight="normal" variant="link" onClick={handleOpenModal}>
+              新しいチームを作成する →
+            </Button>
+          </Box>
         </Box>
       </Flex>
       <TeamCreateModal
